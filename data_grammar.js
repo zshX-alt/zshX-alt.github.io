a8d2bcb3af45e3d6f80fbbc51bcdc8d1d88973be
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-06 16:26:12 +0700

    Create data_grammar.js

---
+++ data_grammar.js
@@ -0,0 +1,17 @@
+const grammarDatabase = [
+    { 
+        pattern: "V (Stem) + たい", 
+        meaning: "Ingin (Saya/Anda)", 
+        example: "日本へ行きたいです。" 
+    },
+    { 
+        pattern: "V-たら", 
+        meaning: "Jika / Setelah", 
+        example: "お金があったら、車を買います。" 
+    },
+    { 
+        pattern: "V-て + おく", 
+        meaning: "Melakukan persiapan (melakukan dulu)", 
+        example: "料理を作っておきます。" 
+    }
+];

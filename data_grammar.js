1165584dec4ef540fa6eb932c8e5570f45a227b5
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 19:13:41 +0700

    Update data_grammar.js

--- data_grammar.js
+++ data_grammar.js
@@ -4,6 +4,20 @@ const N4_GRAMMAR = {
         kanji: "～たい", 
         baca: "V-masu + tai", 
         arti: "Ingin... | Contoh: 寿司を食べたい (Ingin makan sushi)", 
-        type: "grammar", state: 'new' 
+        type: "grammar", state: "new", last: null 
+    },
+    "g02": { 
+        id: "g02", 
+        kanji: "～ながら", 
+        baca: "V-masu + nagara", 
+        arti: "Sambil... | Contoh: 音楽を聞きながら勉強する", 
+        type: "grammar", state: "new", last: null 
+    },
+    "g03": { 
+        id: "g03", 
+        kanji: "～てください", 
+        baca: "V-te + kudasai", 
+        arti: "Tolong... | Contoh: ここに座ってください", 
+        type: "grammar", state: "new", last: null 
     }
 };

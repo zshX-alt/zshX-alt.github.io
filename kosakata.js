d6e06f29db0d92f6ba36243c28348b0d998ee9fd
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 18:12:16 +0700

    Delete kosakata.js

--- kosakata.js
+++
@@ -1,9 +0,0 @@
-// kosakata.js
-const n4Vocab = [
-    { id: 1, kanji: "食べる", reading: "たべる", meaning: "Makan", status: "new", nextReview: "" },
-    { id: 2, kanji: "飲む", reading: "のむ", meaning: "Minum", status: "new", nextReview: "" },
-    { id: 3, kanji: "行く", reading: "いく", meaning: "Pergi", status: "new", nextReview: "" },
-    { id: 4, kanji: "見る", reading: "みる", meaning: "Melihat", status: "new", nextReview: "" },
-    { id: 5, kanji: "毎日", reading: "mainichi", meaning: "Setiap hari", status: "new", nextReview: "" },
-    // Tambahkan terus sampai id: 1500
-];

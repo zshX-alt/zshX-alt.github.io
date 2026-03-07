f9b7983209aac9b365f4affad12278ce0e6997ca
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-07 18:13:03 +0700

    Create data_n4.js

---
+++ data_n4.js
@@ -0,0 +1,15 @@
+const N4_DATASET = {
+    // --- VOCABULARY SECTION ---
+    "v01": { id: "v01", kanji: "通う", baca: "kayou", arti: "pulang pergi", type: "vocab", tags: ["harian"], s: 1, d: 5, last: null, state: 'new' },
+    "v02": { id: "v02", kanji: "乗り換える", baca: "norikaeru", arti: "pindah kendaraan", type: "vocab", tags: ["transportasi"], s: 1, d: 5, last: null, state: 'new' },
+    "v03": { id: "v03", kanji: "資料", baca: "shiryou", arti: "dokumen", type: "vocab", tags: ["kantor"], s: 1, d: 5, last: null, state: 'new' },
+    "v04": { id: "v04", kanji: "予定", baca: "yotei", arti: "rencana", type: "vocab", tags: ["waktu"], s: 1, d: 5, last: null, state: 'new' },
+    "v05": { id: "v05", kanji: "調べる", baca: "shiraberu", arti: "memeriksa", type: "vocab", tags: ["belajar"], s: 1, d: 5, last: null, state: 'new' },
+
+    // --- GRAMMAR SECTION ---
+    "g01": { id: "g01", kanji: "～方がいい", baca: "hou ga ii", arti: "lebih baik...", type: "grammar", tags: ["saran"], s: 1, d: 5, last: null, state: 'new' },
+    "g02": { id: "g02", kanji: "～なければならない", baca: "nakereba naranai", arti: "harus...", type: "grammar", tags: ["keharusan"], s: 1, d: 5, last: null, state: 'new' },
+    "g03": { id: "g03", kanji: "～てもいい", baca: "temo ii", arti: "boleh...", type: "grammar", tags: ["izin"], s: 1, d: 5, last: null, state: 'new' },
+    "g04": { id: "g04", kanji: "～ことができる", baca: "koto ga dekiru", arti: "bisa/dapat...", type: "grammar", tags: ["kemampuan"], s: 1, d: 5, last: null, state: 'new' },
+    "g05": { id: "g05", kanji: "～すぎる", baca: "sugiru", arti: "terlalu...", type: "grammar", tags: ["kondisi"], s: 1, d: 5, last: null, state: 'new' }
+};

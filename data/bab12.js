e8ca4de9acb1bc751f6a36055370741a29b8bed9
zshX-alt <firmansyahhcahya5@gmail.com>
2026-03-16 03:57:45 +0700

    Create bab12.js

---
+++ data/bab12.js
@@ -0,0 +1,39 @@
+const bab12 = {
+    title: "第１２課 (Bab 12) - Perbandingan & Masa Lalu",
+    description: "Belajar membandingkan dua benda dan menceritakan kejadian di masa lampau.",
+    bunpou: [
+        {
+            pola: "1. A wa B yori [Kata Sifat] desu (A lebih ... daripada B)",
+            jp: "<ruby>東京<rt>とうきょう</rt></ruby> は <ruby>大阪<rt>おおさか</rt></ruby> より <ruby>人<rt>ひと</rt></ruby> が <ruby>多<rt>おお</rt></ruby>い です。",
+            ro: "Tokyo wa Osaka yori hito ga ooi desu.",
+            id: "Tokyo lebih ramai orang daripada Osaka."
+        },
+        {
+            pola: "2. A to B to, dochira ga [Kata Sifat] desu ka? (A dan B, mana yang lebih...)",
+            jp: "<ruby>日本料理<rt>にほんりょうり</rt></ruby> と <ruby>中華料理<rt>ちゅうかりょうり</rt></ruby> と, どちら が <ruby>好<rt>す</rt></ruby>き です か。",
+            ro: "Nihon-ryouri to chuuka-ryouri to, dochira ga suki desu ka?",
+            id: "Masakan Jepang dan masakan China, mana yang lebih kamu suka?"
+        },
+        {
+            pola: "3. [Kata Benda] wa [Kategori] ga ichiban [Kata Sifat] desu (Yang paling...)",
+            jp: "この <ruby>料理<rt>りょうり</rt></ruby> は <ruby>全部<rt>ぜんぶ</rt></ruby> の <ruby>中<rt>なか</rt></ruby> で 一番 <ruby>美味<rt>おい</rt></ruby>しい です。",
+            ro: "Kono ryouri wa zenbu no naka de ichiban oishii desu.",
+            id: "Masakan ini adalah yang paling enak di antara semuanya."
+        },
+        {
+            pola: "4. Masa Lalu (V-Mashita / I-Kattara)",
+            jp: "<ruby>昨日<rt>きのう</rt></ruby> は <ruby>暑<rt>あつ</rt></ruby>かった です。",
+            ro: "Kinou wa atsukatta desu.",
+            id: "Kemarin panas."
+        }
+    ],
+    tips: [
+        "**'Yori' vs 'Hodo'**: 'Yori' artinya 'daripada'. Inget, polanya: A wa B yori [Kata Sifat]. Jangan kebalik ya, Bro!",
+        "**'Dochira'**: Dipake buat milih antara dua benda. Kalau pilihannya lebih dari dua, pake 'Dore'.",
+        "**Masa Lalu**: Ingat, kata sifat-i kalau jadi lampau, huruf 'i' terakhir dibuang terus ditambah 'katta'. Contoh: Samui -> Samukatta (Dingin).",
+        "**Pro Tip**: Kalau lagi ngobrol sama orang Jepang, sering-sering bandingin hal-hal simpel pake 'Yori'. Itu bikin kalimat lo kedengeran lebih berisi.",
+        "**Waspada**: Jangan lupa, 'Ichiban' itu buat yang 'paling' (superlatif). Hati-hati salah pakai di antara perbandingan dua benda."
+    ]
+};
+
+export default bab12;
